3e9b182d90bb13e1c57f5af194c57faee49b7f3f
Hyejin1010 <hyepago@naver.com>
2020-03-24 17:22:47 +0900

    MOD: header component logo, account, navbar 추가

--- src/components/header/headerModule.js
+++ src/components/header/headerModule.js
@@ -2,23 +2,22 @@ import React, {Component} from "react";
 import styles from "./header.module.scss";
 import classNames from "classnames/bind";
 
+import { Navbar } from "components";
+
 const cx = classNames.bind(styles);
 
 class headerModule extends Component {
-  constructor (props) {
-    super(props);
-
-    this.state = {
-      isOpen: true
-    };
-  }
-
   render () {
-    const { name } = this.props;
-
     return (
-      <div>
-        {name}
+      <div className={cx('header')}>
+        <div className={cx('account')}>
+          <button className={cx('btn', 'btn-link', 'login')}>로그인</button>
+          <button className={cx('btn', 'btn-link', 'register')}>회원가입</button>
+        </div>
+        <div className={cx('logo')}>
+          <img src="logo.svg" alt="logo"></img>
+        </div>
+        <Navbar></Navbar>
       </div>
     );
   }
