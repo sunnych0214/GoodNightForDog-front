d724a2b87a9c84104d251b9ec2f36f6dc3344f4a
Hyejin1010 <hyepago@naver.com>
2020-03-25 17:44:31 +0900

    FEAT: adoptItem component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from "./header/headerModule";
 export { default as Modal } from "./modal/modalModule";
 export { default as Navbar } from './navbar/navbarModule';
+export { default as AdoptItem } from './adopt-item/adoptItemModule';
