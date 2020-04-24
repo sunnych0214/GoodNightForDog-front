1fb325c9cedbaccba222644da564b656d4e79cd3
sunnych0214 <sunnych0214@gmail.com>
2020-04-24 19:14:16 +0900

    FEAT: create login module

---
+++ src/pages/Login/LoginModule.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+import styles from "./Login.module.scss";
+import classNames from "classnames/bind"
+
+const cx = classNames.bind(styles);
+
+class LoginModule extends Component{
+    render(){
+        return(
+            <div>
+                LOGIN TEST
+            </div>
+        );
+    }
+}
+
+export default LoginModule;
