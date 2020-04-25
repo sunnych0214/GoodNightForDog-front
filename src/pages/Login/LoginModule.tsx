0c1222317ab3c104342e71568bf298fc41f58aab
sunnych0214 <sunnych0214@gmail.com>
2020-04-26 00:18:45 +0900

    MOD: modify with tslint, clarify name of props&state, modify register link

--- src/pages/Login/LoginModule.tsx
+++ src/pages/Login/LoginModule.tsx
@@ -1,4 +1,4 @@
-import React, { Component, FormEvent, ChangeEvent } from "react"
+import React, { Component, FormEvent, ChangeEvent } from "react";
 import styles from "./Login.module.scss";
 import classNames from "classnames/bind";
 import { Link } from 'react-router-dom';
@@ -8,19 +8,19 @@ const cx = classNames.bind(styles);
 const testIdPw = {
     id : 'test',
     pw : 'test1234'
-}
+};
 
-interface LoginInfo {
+interface LoginInfoState {
     id : string;
     pw : string;
 }
 
-interface UserInfo {
+interface UserInfoProps {
     name : string;
 }
 
-class LoginModule extends Component<UserInfo,LoginInfo>{
-    constructor(props : UserInfo){
+class LoginModule extends Component<UserInfoProps,LoginInfoState>{
+    constructor(props : UserInfoProps){
         super(props);
 
         this.onIdChange = this.onIdChange.bind(this);
@@ -29,15 +29,12 @@ class LoginModule extends Component<UserInfo,LoginInfo>{
         this.state = {
             id : '',
             pw : ''
-        }
+        };
     }
-    
-    
 
     onIdChange = (e:ChangeEvent<HTMLInputElement>) => {
         this.setState({id:e.target.value});
     }
-    
     onPwChange = (e:ChangeEvent<HTMLInputElement>) => {
         this.setState({pw:e.target.value});
     }
@@ -45,15 +42,14 @@ class LoginModule extends Component<UserInfo,LoginInfo>{
 
     onSubmit = (e:FormEvent<HTMLFormElement>):void => {
         const { id, pw } = this.state;
-        console.log("id : "+id+" pw : "+pw);
 
-        //todo : id,pw check, print red error line
+        /* id,pw check, print red error line */
         if(id !== testIdPw.id){
-            //id not exist
+            /* id not exist */
             alert("존재하지 않는 아이디입니다!");
         }
         else if(pw !== testIdPw.pw){
-            //password wrong
+            /* password wrong */
             alert("비밀번호를 다시 한 번 확인해 주세요");
         }
     }
@@ -61,7 +57,6 @@ class LoginModule extends Component<UserInfo,LoginInfo>{
     render(){
         const { id, pw } = this.state;
         const { onIdChange, onPwChange } = this;
-        
         return(
             <div className={cx('box-little')}>
                 <h2 className={cx('box-little-title')}>로그인</h2><p/>
@@ -79,7 +74,7 @@ class LoginModule extends Component<UserInfo,LoginInfo>{
                         찾기
                     </span>
                     <span className={cx('span-little float-right')}>
-                        <Link to="/signin">회원가입</Link>
+                        <Link to="/register">회원가입</Link>
                     </span><br/>
                     <span className={cx('span-little')}>
 
