b97dee0bbf80be00bb0d1b46c3de15895564a703
WHYKIKII <cintamani_whyk@naver.com>
2020-05-10 03:39:16 +0900

    MOD : 아이디 비밀번호 유효성검사, 중복체크 버튼, 비밀번호 확인

--- src/pages/Register/RegisterPage.tsx
+++ src/pages/Register/RegisterPage.tsx
@@ -1,37 +1,106 @@
-import React from 'react';
+import React, { Component, ChangeEvent } from 'react';
 import styles from './RegisterPage.module.scss';
 import classnames from 'classnames/bind';
 
 const cx = classnames.bind(styles);
 
-const RegisterPage = () => {
-    return (
-        <div className={cx('container')}>
-            <h2>회원가입</h2>
+interface RegisterInfo {
+    email : string,
+    id : string,
+    idReg : boolean,
+    pw : string,
+    pwReg : boolean,
+    pwConfirm : boolean
+}
 
-            <div className={cx('label')}>이메일</div>
-            <div>
-                <input name="email" placeholder="이메일"/> 
-            </div>
+class RegisterPage extends React.Component<RegisterInfo> {
+    state:RegisterInfo = {
+        email : '',
+        id : '',
+        idReg : false,
+        pw : '',
+        pwReg : false,
+        pwConfirm : true
+    }
+    
+    onIdChange = (e:ChangeEvent<HTMLInputElement>) => {
+        let idRegExp = /^[A-za-z0-9]{5,20}/g;
+        
+        if(idRegExp.test(e.target.value)) {
+            // 정규식 통과
+            this.setState(state => ({id:e.target.value}));
+            this.setState({idReg:true});
+        } else {
+            // 정규식 불통과
+            this.setState({idReg:false});
+        }
+    }
 
-            <div className={cx('label')}>아이디</div>
-            <div>
-                <input name="usrId" placeholder="아이디"/> 
-            </div>
+    idDuplicate = (e:ChangeEvent<HTMLInputElement>) => {
+            // DB 체크
+    }
 
-            <div className={cx('label')}>비밀번호</div>
-            <div>
-                <input name="passwd" type="password" placeholder="비밀번호"/> 
-            </div>
+    onPwChange = (e:ChangeEvent<HTMLInputElement>) => {
+        let pwRegExp = /^[A-za-z0-9]{8,20}/g;
+        let pwVal = e.target.value;
+        
+        if(pwRegExp.test(pwVal)) {
+            // 정규식 통과
+            this.setState(state => ({pw:pwVal}));
+            this.setState({pwReg:true});
+        } else {
+            // 정규식 불통과
+            this.setState({pwReg:false});
+        }
 
-            <div className={cx('label')}>비밀번호 확인</div>
-            <div>
-                <input name="passwd" type="password" placeholder="비밀번호 확인"/> 
+        this.setState({pwConfirm:false});
+    }
+
+    isPwEqaul = (e:ChangeEvent<HTMLInputElement>) => {
+        if(this.state.pw === e.target.value) {
+            this.setState({pwConfirm:true});   
+        } else {
+            this.setState({pwConfirm:false});
+        }
+    }
+
+
+    render() {
+        return (
+            <div className={cx('container')}>
+            <div className={cx('register-box')}>
+                <h2 className={cx('register-title')}>회원가입</h2>
+
+                <form className={cx('register-form')}>
+                    <span className={cx('register-label')}>이메일</span><br/>
+                    <input className={cx('register-input')} type="email" name="email" placeholder="이메일" required/> <p/>
+
+                    <span className={cx('register-label')}>아이디</span><br/>
+                    <input className={cx('register-input-id')} name="id" placeholder="아이디" onChange={this.onIdChange} required/>
+                    <button className={cx('btn-duplicate-check')}>중복체크</button><br/>
+                    { this.state.idReg ? <span></span> : 
+                        <span className={cx('Reg-Nonpass')}>아이디는 5글자 이상이여야 하며 영문과 숫자를 반드시 포함해야 합니다.</span> }
+                     <p/>
+
+                    <span className={cx('register-label')}>비밀번호</span><br/>
+                    <input className={cx('register-input')} name="pw" type="password" placeholder="비밀번호" onChange={this.onPwChange} required/>
+                    { this.state.pwReg ? <span></span> : 
+                        <span className={cx('Reg-Nonpass')}>비밀번호는 8글자 이상이여야 하며 영문과 숫자를 반드시 포함해야 합니다.</span> }
+                    <p/>
+
+                    <span className={cx('register-label')}>비밀번호 확인</span><br/>
+                    <input className={cx('register-input')} name="pwConfirm" type="password" placeholder="비밀번호 확인" onChange={this.isPwEqaul} required/> 
+                    { this.state.pwConfirm ? <span></span> : 
+                        <span className={cx('Reg-Nonpass')}>비밀번호가 일치하지 않습니다.</span> }
+                    <p/>
+                    
+                    <input type="submit" value="회원가입" className={cx('register-submit')}/>
+                </form>
             </div>
-            
-            <button className={cx('btn', 'register')}>가입완료</button>
         </div>
-    )
+        )
+    }
+
 }
 
 export default RegisterPage;
