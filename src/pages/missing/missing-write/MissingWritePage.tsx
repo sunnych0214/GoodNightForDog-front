c35249740c91beb5b6133b54857fd7b9f8f50794
Hyejin1010 <hyepago@naver.com>
2020-06-13 23:38:03 +0900

    MOD: 견종 리스트를 따로 뺌

--- src/pages/missing/missing-write/MissingWritePage.tsx
+++ src/pages/missing/missing-write/MissingWritePage.tsx
@@ -2,6 +2,7 @@ import React, { Component, ChangeEvent } from 'react';
 import styles from './MissingWritePage.module.scss';
 import classnames from 'classnames/bind';
 import { Dropdown, DropdownProps } from 'semantic-ui-react';
+import { BREEDS } from '../../../data';
 
 
 const cx = classnames.bind(styles);
@@ -39,12 +40,7 @@ class MissingWritePage extends Component<MissingWritePageProps, MissingWritePage
 
         this.state = { missing: { gender: 'male', images: ['', '', ''] } } as MissingWritePageState;
 
-        ['골든 리트리버', '꼬똥 드 튤리어', '닥스훈트', '달마시안', '도베르만', '래브라도 리트리버'
-        , '말라뮤트', '말티즈', '믹스견', '베들링턴 테리어', '보더콜리', '보스턴테리어', '불독', '비글'
-        , '바숑프리제', '사모예드', '샤페드', '세인트 버나드', '셔틀랜드 쉽독', '셰퍼드', '슈나우저', '스피치'
-        , '시바 이누', '시베리안 허스키', '시추', '아프간하운드', '올드 잉글리시 쉽독', '요크셔테리어', '웰시코기'
-        , '재패니스 친', '진도견', '치우차우', '치와와', '코카스파니엘', '콜리', '파피용', '퍼크', '페키니즈'
-        , '포메라니안', '푸들', '프렌치 불독', '하바네즈', '기타'].forEach((value: string, index: number) => {
+        BREEDS.forEach((value: string, index: number) => {
             this.breeds.push({ key: index, text: value, value });
         });
     }
