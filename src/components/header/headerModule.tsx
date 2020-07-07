27179a2b029c072a7980ee727c35b27aef9bd490
Hyejin1010 <hyepago@naver.com>
2020-07-07 17:44:21 +0900

    MOD: Header Code 정리

--- src/components/header/headerModule.tsx
+++ src/components/header/headerModule.tsx
@@ -21,23 +21,9 @@ class HeaderComponent extends Component<IRecipeProps> {
 
   render () {
     return (
-      <div className={cx('header')}>
-        {/*
-        <div className={cx('account')}>
-          <Link to="/login">
-            <button className={cx('btn', 'btn-link', 'login')}>로그인</button>
-          </Link>
-          <button className={cx('btn', 'btn-link', 'register')}>회원가입</button>
-          <Link to="/my-page">
-            <button className={cx('btn', 'btn-link')}>마이페이지</button>
-          </Link>
-        </div>
-        <div className={cx('logo')}>
-          <Link to="/"><img src="logo.svg" alt="logo" /></Link>
-        </div>
-        */}
+      <header>
         <Navbar></Navbar>
-      </div>
+      </header>
     );
   }
 }
